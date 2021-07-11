a973b1aa6a2d94efdf24cd62d8e8c822d06b4e0a
omar alshobky <93231975za@gmail.com>
2021-07-11 18:12:02 +0200

    تحوبل من قائمة

---
+++ MyHasaby/MyHasaby/Uesr.cs
@@ -0,0 +1,29 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyHasaby
+{
+    public class User
+    {
+        readonly SQLiteAsyncConnection _database;
+
+        public User(string dbPath)
+        {
+            _database = new SQLiteAsyncConnection(dbPath);
+            _database.CreateTableAsync<Users>().Wait();
+        }
+
+        public Task<List<Users>> GetPeopleAsync()
+        {
+            return _database.Table<Users>().ToListAsync();
+        }
+
+        public Task<int> SavePersonAsync(Users person)
+        {
+            return _database.InsertAsync(person);
+        }
+    }
+}
---
+++ MyHasaby/MyHasaby/Users.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace MyHasaby
+{
+   public class Users
+    {
+        [Indexed]
+        public int Id { get; set; }
+        public int Dane { get; set; }
+        public int Mdan { get; set; }
+
+    }
+}
