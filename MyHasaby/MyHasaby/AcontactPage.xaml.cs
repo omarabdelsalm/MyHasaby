d7271cc831560e7f6d9ccef2acce05cabef15d53
omar alshobky <93231975za@gmail.com>
2021-09-19 18:52:33 +0200

    وسائل اتصال

--- MyHasaby/MyHasaby/AcontactPage.xaml.cs
+++ MyHasaby/MyHasaby/AcontactPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Plugin.Messaging;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -48,6 +49,46 @@ namespace MyHasaby
             
 
 
+
+        }
+
+        private void Smsbtn(object sender, EventArgs e)
+        {
+            // Send Sms
+            var smsMessenger = CrossMessaging.Current.SmsMessenger;
+            if (smsMessenger.CanSendSms)
+                smsMessenger.SendSms("01069160569");
+        }
+
+        private void Emlbtn(object sender, EventArgs e)
+        {
+            
+            var emailMessenger = CrossMessaging.Current.EmailMessenger;
+            if (emailMessenger.CanSendEmail)
+            {
+                // Send simple e-mail to single receiver without attachments, bcc, cc etc.
+                emailMessenger.SendEmail("93231975za@gmail.com", "DevSolm", "DevSolm في خدمتكم طاب يومكم بكل خير");
+
+                // Alternatively use EmailBuilder fluent interface to construct more complex e-mail with multiple recipients, bcc, attachments etc.
+                var email = new EmailMessageBuilder()
+                  .To("93231975za@gmail.com")
+                  .Cc("cc.plugins@xamarin.com")
+                  .Bcc(new[] { "bcc1.plugins@xamarin.com", "bcc2.plugins@xamarin.com" })
+                  .Subject("")
+                  .Body("ابوبكر في خدمتكم طاب يومكم بكل خير")
+                  .Build();
+
+                emailMessenger.SendEmail(email);
+            }
+        }
+
+        private void Callbtn(object sender, EventArgs e)
+        {
+            
+            // Make Phone Call
+            var phoneDialer = CrossMessaging.Current.PhoneDialer;
+            if (phoneDialer.CanMakePhoneCall)
+                phoneDialer.MakePhoneCall("01069160569", "bkr alshobky");
 
         }
     }
