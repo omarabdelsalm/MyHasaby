38e451434869bbc370cf7d766d7546a02b544214
omar alshobky <93231975za@gmail.com>
2021-12-20 21:54:06 +0200

    some adding

--- MyHasaby/MyHasaby/App.xaml.cs
+++ MyHasaby/MyHasaby/App.xaml.cs
@@ -7,14 +7,14 @@ using System.Linq;
 using Xamarin.Forms.Xaml;
 using System.Collections.Generic;
 using Xamarin.Essentials;
-
+[assembly: ExportFont("NotoNaskhArabic-Regular.ttf")]
 namespace MyHasaby
 {
     public partial class App : Application
     {
        
         static Database database;
-        static Uesr database1;
+        static Uesrs database1;
         static AcountUes useAcount;
 
         public static AcountUes acountUes
@@ -39,13 +39,13 @@ namespace MyHasaby
                 return database;
             }
         }
-        public static Uesr User1
+        public static Uesrs User1
         {
             get
             {
                 if (database1 == null)
                 {
-                    database1 = new Uesr(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "people.db3"));
+                    database1 = new Uesrs(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "people.db3"));
                 }
                 return database1;
             }
@@ -83,7 +83,7 @@ namespace MyHasaby
                var ally = App.acountUes.GetAcontactAsync().Result;
                var alhmed = ally.Count();
 
-               if (all2 <= 25)
+               if (all2 <= 3)
                { MainPage = new ShellPage(); }
                else if (alhmed!=0) {
                    
@@ -98,160 +98,33 @@ namespace MyHasaby
                }
            });
 
-                
-                    
-                
-                
+
 
 
             
 
 
-           
-
-            Device.StartTimer(new TimeSpan(0, 0, 60*60*2), () =>
-            {
-                // do something every 60 seconds
-                Device.BeginInvokeOnMainThread(async() =>
-                {
-                    // interact with UI elements
-                    
-                    try
-                    {
-                       // string _dbpath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "people.db3");
-                       // var db=new SQLiteConnection();
-                        var statusWrite = await Permissions.RequestAsync<Permissions.StorageWrite>();
-                        var statusRead = await Permissions.RequestAsync<Permissions.StorageRead>();
-                       // var db = new SQLiteConnection(_dbpath);
-                        string docFolder = Path.Combine(System.Environment.GetFolderPath
-                             (System.Environment.SpecialFolder.MyDocuments), "logs");
-                        string szRestorePath = "/storage/emulated/0/Android/datacom.alshobky.myhasaby/files/logs/temp.db3";
-                        string libFolder = Path.Combine(docFolder, szRestorePath);
-                        if (!Directory.Exists(libFolder))
-                        {
-                            Directory.CreateDirectory(libFolder);
-                        }
-
-
-                        string destinationDatabasePath = Path.Combine(libFolder, $"temp{DateTime.Now.ToString("dd-yy-mm")}.db3");
-
-                        db.Backup(destinationDatabasePath, "main");
 
 
 
-                        return;
-                    }
-                    catch (Exception ex)
-                    {
-                        await Application.Current.MainPage.DisplayAlert("محاولة مرةاخرى", "no", "om");
 
-                    }
-
-                });
-                return true; // runs again, or false to stop
-            });
 
 
         }
 
-        
+
         protected override void OnStart()
         {
-           
+            
         }
 
         protected override void OnSleep()
         {
-            Device.StartTimer(new TimeSpan(0, 0, 60 * 60 * 6), () =>
-            {
-                // do something every 60 seconds
-                Device.BeginInvokeOnMainThread(async () =>
-                {
-                    // interact with UI elements
-
-                    try
-                    {
-                        string _dbpath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "people.db3");
-                        var db = new SQLiteConnection(_dbpath);
-                        var statusWrite = await Permissions.RequestAsync<Permissions.StorageWrite>();
-                        var statusRead = await Permissions.RequestAsync<Permissions.StorageRead>();
-                        // var db = new SQLiteConnection(_dbpath);
-                        string docFolder = Path.Combine(System.Environment.GetFolderPath
-                             (System.Environment.SpecialFolder.MyDocuments), "logs");
-                        string szRestorePath = "/storage/emulated/0/Android/datacom.alshobky.myhasaby/files/logs/temp.db3";
-                        string libFolder = Path.Combine(docFolder, szRestorePath);
-                        if (!Directory.Exists(libFolder))
-                        {
-                            Directory.CreateDirectory(libFolder);
-                        }
-
-
-                        string destinationDatabasePath = Path.Combine(libFolder, $"temp{DateTime.Now.ToString("dd-yy-mm")}.db3");
-
-                        db.Backup(destinationDatabasePath, "main");
-
-
-
-                        //await Application.Current.MainPage.DisplayAlert("حفط نسخة احتياطية", "مسار الحفظ Android/datacom.alshobky.myhasaby/files/logs/temp.db3", "OK");
-                        return;
-                    }
-                    catch (Exception ex)
-                    {
-                        await Application.Current.MainPage.DisplayAlert("محاولة مرةاخرى", "no", "om");
-
-                    }
-
-                });
-                return true; // runs again, or false to stop
-            });
-
-
+            
         }
 
         protected override void OnResume()
         {
-            Device.StartTimer(new TimeSpan(0, 0, 60 * 60 * 6), () =>
-            {
-                // do something every 60 seconds
-                Device.BeginInvokeOnMainThread(async () =>
-                {
-                    // interact with UI elements
-
-                    try
-                    {
-                        string _dbpath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "people.db3");
-                        var db = new SQLiteConnection(_dbpath);
-                        var statusWrite = await Permissions.RequestAsync<Permissions.StorageWrite>();
-                        var statusRead = await Permissions.RequestAsync<Permissions.StorageRead>();
-                        // var db = new SQLiteConnection(_dbpath);
-                        string docFolder = Path.Combine(System.Environment.GetFolderPath
-                             (System.Environment.SpecialFolder.MyDocuments), "logs");
-                        string szRestorePath = "/storage/emulated/0/Android/datacom.alshobky.myhasaby/files/logs/temp.db3";
-                        string libFolder = Path.Combine(docFolder, szRestorePath);
-                        if (!Directory.Exists(libFolder))
-                        {
-                            Directory.CreateDirectory(libFolder);
-                        }
-
-
-                        string destinationDatabasePath = Path.Combine(libFolder, $"temp{DateTime.Now.ToString("dd-yy-mm")}.db3");
-
-                        db.Backup(destinationDatabasePath, "main");
-
-
-
-                        //await Application.Current.MainPage.DisplayAlert("حفط نسخة احتياطية", "مسار الحفظ Android/datacom.alshobky.myhasaby/files/logs/temp.db3", "OK");
-                        return;
-                    }
-                    catch (Exception ex)
-                    {
-                        await Application.Current.MainPage.DisplayAlert("محاولة مرةاخرى", "no", "om");
-
-                    }
-
-                });
-                return true; // runs again, or false to stop
-            });
 
         }
     }
