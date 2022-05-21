53c3b5f604d9c8f655936103b2fbcd99a978b4fd
omar alshobky <93231975za@gmail.com>
2022-05-21 16:46:06 +0200

    test add

---
+++ MyHasaby/MyHasaby.Android/AccessFileImplement.cs
@@ -0,0 +1,104 @@
+﻿using Android;
+using Android.App;
+using Android.Content;
+using Android.Gms.Tasks;
+using Android.OS;
+using Android.Provider;
+using Android.Runtime;
+using Android.Support.V4.Content;
+using Android.Views;
+using Android.Widget;
+using Java.IO;
+using MyHasaby.Droid;
+using Plugin.CurrentActivity;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using static Android.Provider.SyncStateContract;
+
+using Xamarin.Forms;
+using File = Java.IO.File;
+
+
+[assembly: Xamarin.Forms.Dependency(typeof(AccessFileImplement))]
+namespace MyHasaby.Droid
+{
+    //method to get backup file use with android 10 and android 11
+    public class AccessFileImplement : IAccessFileService
+    {
+        
+        public void CreateFile(string FileName)
+        {
+
+            // //   Permissions.RequestAsync<Permissions.StorageWrite>();
+            // //   Permissions.RequestAsync<Permissions.StorageRead>();
+            string _dbpath = Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.LocalApplicationData), "people.db3");
+
+            try
+            {
+                File folder = new File(Android.OS.Environment.GetExternalStoragePublicDirectory("/Download/") + "/" + "Myhasaby");
+                var isfolder = false;
+                if (!folder.Exists())
+                {
+                    isfolder = folder.Mkdir();
+                }
+                string filename = $"temp{DateTime.Now.ToString("dd-MM-yyyy")}.db3";
+                string destinationDatabasePath = Path.Combine(folder.ToString(), filename);
+                var db = new SQLiteConnection(_dbpath);
+                db.Backup(destinationDatabasePath, "main");
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine("Failed to get data back from content resolver. Filename: " + FileName);
+                System.Diagnostics.Debug.WriteLine("Exception: " + ex.Message);
+                System.Diagnostics.Debug.Flush();
+                //return false;
+
+
+
+            }
+
+        }
+    //function to make backup for android 11
+        public string CreateFile1()
+        {
+            
+                // Do things the Lollipop way
+                string _dbpath = Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.LocalApplicationData), "people.db3");
+
+            File folder = new File(Android.OS.Environment.GetExternalStoragePublicDirectory("/Download/") + "/" + "Myhasaby");
+
+            var isfolder = false;
+                if (!folder.Exists())
+                {
+                    isfolder = folder.Mkdir();
+                }
+                // var file = FilePicker.PickAsync();
+
+                string filename = $"temp{DateTime.Now.ToString("dd-MM-yyyy")}.db3";
+                string destinationDatabasePath = Path.Combine(folder.ToString(), filename);
+
+                return destinationDatabasePath;
+           
+  }
+        //function to make backup for android 11
+
+        public string copy()
+        {
+            string _dbpath = Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.LocalApplicationData), "people.db3");
+
+            File folder = new File(Android.OS.Environment.GetExternalStoragePublicDirectory("Download") + "/" + "Myhasaby");
+            string filename = $"temp{DateTime.Now.ToString("dd-MM-yyyy")}.db3";
+            string destinationDatabasePath = Path.Combine(folder.ToString(), filename);
+              return destinationDatabasePath;
+          }
+       
+ 
+    }
+
+}
