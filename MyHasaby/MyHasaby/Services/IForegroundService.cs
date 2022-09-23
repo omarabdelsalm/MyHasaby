0a4913bb5a1f329289fe84625c4b13a97a6b3963
omar alshobky <93231975za@gmail.com>
2022-09-23 18:28:39 +0200

    first commit

---
+++ MyHasaby/MyHasaby/Services/IForegroundService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyHasaby.Services
+{
+    public interface IForegroundService
+    {
+        void StartMyForegroundService();
+        void StopMyForegroundService();
+
+        bool IsForeGroundServiceRunning();
+    }
+}
